cc942090cb6bb6aca75d6310ea77a4e71a2b726d
MaisaniSimone <maisanisimone03@gmail.com>
2022-03-19 22:49:35 +0100

    Prima schermata

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,40 +1,44 @@
 import * as React from 'react';
 
+import { useState } from 'react';
 import './App.css';
+import Opzioni from './Opzioni';
 import foto from "./img/logo.png";
 import imagine from "./audio/imagine.mp3";
+import Classe5C from './Classe5C';
+
+
 
 function App() {
-
+  const [FinestraOpt, setFinestraOpt] = useState("Home")
  
  
   const Apri=(apri, chiudi)=> {
 
     document.getElementById(apri).style.display = "flex"
     document.getElementById(chiudi).style.display = "none"
+    setFinestraOpt(chiudi)
   }
-  const volume=()=>{
-    document.getElementById("cs_audio").volume=document.getElementById("Volume").value/100
-  }
+  
   return (
 
     <div id='Contenitore'>
       
-      <img id="logo" src={foto} width="80%" ></img>
       <div id="Home" class="Finestra">
+      <img class="logo" src={foto} width="80%" ></img>
         <div id="pulsanti">
-          <input id='play' type="button" value="PLAY"/>
-          <input id='option' type="button" value="OPTION"  onClick={()=>Apri("Option","Home") }/>
+          <input id='play' type="button" value="PLAY"  onClick={()=>Apri("Gioco","Home") }/>
+          <input class='option' type="button" value="OPTION"  onClick={()=>Apri("Option","Home") }/>
         </div>
       </div>
-      <div id="Option" class="Finestra">
-        <div id='contVol'>
-        <p id='txtVolume'>Volume</p>
-        <input type="range" id="Volume" onChange={()=>{volume()}}/>
-        </div>
-        
-        <input type="button" id="back" value="Torna al Menu" onClick={()=>Apri("Home","Option")}/>
-       
+      <Opzioni from={FinestraOpt}/>
+      
+      <div id="Gioco" class="Finestra">
+        <Classe5C />
+        <div id="pulsantiGioco">
+            <input type="button" class="back" value="Torna al Menu" onClick={()=>Apri("Home","Gioco")}/>
+            <input class='back' type="button" value="OPTION"  onClick={()=>Apri("Option","Gioco") }/>
+        </div>      
       </div>
       <audio id="cs_audio" loop controls autoPlay hidden="true">
         <source src={imagine} type="audio/mpeg"/>
