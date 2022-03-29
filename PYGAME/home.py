e43463c40076fbc325b393f4c33d88bedaa54aed
MaisaniSimone <maisanisimone03@gmail.com>
2022-03-29 20:07:59 +0200

    Gestione movimento pygame

--- PYGAME/home.py
+++ PYGAME/home.py
@@ -1,14 +1,22 @@
+
 import pygame,sys
 pygame.init() 
 
 clock = pygame.time.Clock()
-clock.tick(90)
+clock.tick(60)
+global oggetti
+oggetti=[]
 
 #tutte le immagini da inizializzare
 icona = pygame.image.load('img/fantasmaPixel.png') 
 sfondo=pygame.image.load("img/sfondo.jpeg")
 sfondoGioco=pygame.image.load("img/5C.jpg")
 logo=pygame.image.load("img/logo.png")
+banco=pygame.image.load("img/banchi.jpg")
+armadio=pygame.image.load("img/armadio.jpg")
+cestini=pygame.image.load("img/cestini.jpg")
+lavagna=pygame.image.load("img/lavagna.jpg")
+cattedra=pygame.image.load("img/cattedra.jpg")
 logo=pygame.transform.smoothscale(logo, (772, 222))
 
 global schermata
@@ -16,6 +24,21 @@ schermata="Home"
 
 global run
 run=True
+
+class Oggetto:
+    def __init__(self,img,width,heigth,x,y):
+        self.img=img
+        self.heigth=heigth
+        self.width=width
+        self.x=x
+        self.x2=x+width
+        self.y=y
+        self.y2=y+heigth
+        oggetti.append(self)
+        
+         
+    def draw(self):
+        SCREEN.blit(self.img,(self.x,self.y))
 
 class Button:
         
@@ -79,23 +102,23 @@ class Button:
 
 class Soul:
     def __init__(self):
-        self.x=300
-        self.y=300
+        self.x=600
+        self.y=100
         self.image=pygame.image.load("img/fantasmaPixel.png")
         self.velx=0
         self.vely=0
 
     def vaiSu(self):
-        self.vely-=1
+        self.vely-=4
 
     def vaiGiu(self):
-        self.vely+=1
+        self.vely+=4
 
     def vaiDx(self):
-        self.velx+=1
+        self.velx+=4
 
     def vaiSx(self):
-        self.velx-=1
+        self.velx-=4
 
     def ferma(self):
         self.vely=0
@@ -148,8 +171,8 @@ def opzioni():
 def gioco():
     SCREEN.blit(sfondo,(0,0))
     SCREEN.blit(sfondoGioco,(200,0))
-    personaggio.x+=personaggio.velx
-    personaggio.y+=personaggio.vely
+    for oggetto in oggetti:
+        oggetto.draw()
     SCREEN.blit(personaggio.image,(personaggio.x,personaggio.y))
     for b in buttons:
         if b.opt==2:
@@ -182,6 +205,8 @@ pygame.mixer.music.set_volume(0.500)
 SCREEN = pygame.display.set_mode((1071,770))
 pygame.display.set_icon(icona)
 pygame.display.set_caption("SOUL'ENIGMISTA")
+
+
 gui_font = pygame.font.Font(None,30)
 buttons=[]
 aOpt=apriOpzioni
@@ -201,22 +226,57 @@ Button(4,'#000000','#333333','+',40,40,(450,250),5,alz)
 Button(4,'#000000','#333333','-',40,40,(570,250),5,abb)
 Button(4,'#007FFF','#0066CC','Torna al gioco',200,40,(440,300),5,aGioco)
 
+Oggetto(armadio,32,64,808,160)
+Oggetto(cestini,64,64,232,32)
+Oggetto(lavagna,64,64,520,-5)
+Oggetto(cattedra,64,96,712,32)
+xO=135
+yO=160
+x=xO
+y=yO
+
+for i in range(1,20):
+
+    if(i==3 or i==7 or i==11 or i==15):
+        x+=160
+        Oggetto(banco,32,64,x,y)
+        x=xO
+        y+=96
+    else:
+        x+=160
+        Oggetto(banco,32,64,x,y)
+
 global personaggio
 personaggio=Soul()
 
 while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
     
     if schermata == "Home":
         home()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
     elif schermata == "Opzioni":
         opzioni()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
     elif schermata == "Gioco":
+        posx=personaggio.x+personaggio.velx
+        posy=personaggio.y+personaggio.vely
+        move=0
+        if(posx<200+633 and posx>200 and posy>32 and posy<580):
+            for oggetto in oggetti:
+                if(posx>oggetto.x-20 and posx<oggetto.x2-20 and posy>oggetto.y-30 and posy<oggetto.y2-20):
+                    move=1
+            if (move==0):
+                personaggio.x+=personaggio.velx
+                personaggio.y+=personaggio.vely
         gioco()
         for evento in pygame.event.get():
+
             if(evento.type == pygame.KEYUP and (evento.key == pygame.K_UP or evento.key == pygame.K_DOWN or evento.key == pygame.K_RIGHT or evento.key == pygame.K_LEFT)):
                     personaggio.ferma()
             elif(evento.type == pygame.KEYDOWN):
@@ -228,7 +288,9 @@ while run:
                     personaggio.vaiDx()
                 elif(evento.key == pygame.K_LEFT):
                     personaggio.vaiSx()
-            
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
                             
                     
     elif schermata == "opzioniGioco":
